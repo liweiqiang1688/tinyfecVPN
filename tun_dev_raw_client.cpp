f652b6ceb8f2bad0bc57d2a80419dbddf564372c
Weiqiang Li <weiqiangli@WeiqiangdeMacBook-Air.local>
2026-07-13 00:30:01 +0800

    Fix: local_addr is uninitialized for client, pass default 0.0.0.0:0
    The client does not use -l flag, so local_addr remains uninitialized.
    Calling local_addr.get_str() crashes on assert(is_vaild()) because
    address_t has no default constructor. Pass a default local bind
    address '0.0.0.0:0' instead.

--- tun_dev_raw_client.cpp
+++ tun_dev_raw_client.cpp
@@ -132,7 +132,7 @@ int tun_dev_raw_client_event_loop() {
     conn_info_t *conn_info_p = new conn_info_t;
     conn_info_t &conn_info = *conn_info_p;
 
-    raw_ctx = raw_client_init(remote_addr.get_str(), local_addr.get_str(),
+    raw_ctx = raw_client_init(remote_addr.get_str(), "0.0.0.0:0",
                                key_string, tun_dev);
     if (!raw_ctx) {
         mylog(log_fatal, "raw_client_init failed\n");
