52c0115614691f0d34001c17dc974c0fa7b3cbed
Weiqiang Li <weiqiangli@WeiqiangdeMacBook-Air.local>
2026-07-13 07:51:22 +0800

    Test: use auth_none to skip MD5 auth check

--- tun_dev_raw.cpp
+++ tun_dev_raw.cpp
@@ -181,6 +181,7 @@ raw_client_t *raw_client_init(const char *remote_addr_str, const char *local_add
 
     srand(get_true_random_number_nz());
     const_id = get_true_random_number_nz();
+    auth_mode = auth_none;
     my_init_keys(key_string, 1);
     return ctx;
 }
@@ -333,6 +334,7 @@ raw_server_t *raw_server_init(const char *local_addr_str, const char *key, const
     if (key && key[0]) strncpy(key_string, key, sizeof(key_string) - 1);
     if (dev_name && dev_name[0]) strncpy(dev, dev_name, sizeof(dev) - 1);
     srand(get_true_random_number_nz()); const_id = get_true_random_number_nz();
+    auth_mode = auth_none;
     my_init_keys(key_string, 0);
     return ctx;
 }
