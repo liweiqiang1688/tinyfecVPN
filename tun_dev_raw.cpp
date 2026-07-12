6b3571b719bbfa74344478bf840619a88197ef8c
Weiqiang Li <weiqiangli@WeiqiangdeMacBook-Air.local>
2026-07-13 01:16:35 +0800

    Debug: log cipher_mode value after setting to xor

--- tun_dev_raw.cpp
+++ tun_dev_raw.cpp
@@ -188,9 +188,10 @@ raw_client_t *raw_client_init(const char *remote_addr_str, const char *local_add
     if (dev_name && dev_name[0]) strncpy(dev, dev_name, sizeof(dev) - 1);
     srand(get_true_random_number_nz());
     const_id = get_true_random_number_nz();
-    extern cipher_mode_t cipher_mode;
     cipher_mode = cipher_xor;
     my_init_keys(key_string, 1);
+
+    mylog(log_info, "cipher_mode=%d\n", (int)cipher_mode);
     return ctx;
 }
 void raw_client_destroy(raw_client_t *ctx) { if (ctx) delete ctx; }
@@ -344,9 +345,9 @@ raw_server_t *raw_server_init(const char *local_addr_str, const char *key, const
     if (key && key[0]) strncpy(key_string, key, sizeof(key_string) - 1);
     if (dev_name && dev_name[0]) strncpy(dev, dev_name, sizeof(dev) - 1);
     srand(get_true_random_number_nz()); const_id = get_true_random_number_nz();
-    extern cipher_mode_t cipher_mode;
     cipher_mode = cipher_xor;
     my_init_keys(key_string, 0);
+    mylog(log_info, "cipher_mode=%d\n", (int)cipher_mode);
     return ctx;
 }
 void raw_server_destroy(raw_server_t *ctx) { if (ctx) delete ctx; }
